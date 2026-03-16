14a323c99dd4bafcf252826a1728bee65eff0075
mdtonmoyhasansiam <tonmoy.hasan13534@gmail.com>
2026-03-16 13:16:22 +0600

    JS Comments

---
+++ 004_JS_Comments/app.js
@@ -0,0 +1,25 @@
+// My First Comment
+
+/*
+ * Multi Line Comment
+? Some info
+! Waring
+TODO: Some thing I need to fix
+ */
+
+let firstName = 'Siam';
+let age = 30;
+
+// firstName = 'Siam'; // Changing firstName
+FirstName = 'Tonmoy';
+firstname = 'Hasan';
+/*
+console.log(firstName);
+
+console.log(age + 50);
+*/
+
+function funcName() {
+  console.log('My First Function');
+}
+funcName();
