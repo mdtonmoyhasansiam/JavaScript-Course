237a3c0cdb391ab722462a34fccaade4c599dcea
mdtonmoyhasansiam <tonmoy.hasan13534@gmail.com>
2026-03-16 13:13:30 +0600

    JS_Syntax

---
+++ 003_JS_Syntax/app.js
@@ -0,0 +1,15 @@
+let firstName = 'Hasan';
+let age = 30;
+
+firstName = 'Tonmoy';
+FirstName = 'MD.';
+firstname = 'Siam';
+
+console.log(firstName);
+
+console.log(age + 50);
+
+function funcName() {
+  console.log('My First Function');
+}
+funcName();
